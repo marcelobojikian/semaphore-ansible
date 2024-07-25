1a2c9daa603937ee29ddc51bb491ce977d838886
Marcelo Nogueira <marcelo.bojikian@gmail.com>
2024-07-25 03:07:38 +0100

    Fix decrypt

--- scripts/decrypt.sh
+++ scripts/decrypt.sh
@@ -3,5 +3,7 @@
 FOLDER=.
 VAULT_FILE=/var/security/ansible/vault.key
 
-echo Encrypt proxmox folder
+for var_file in $FOLDER/*/group_vars/*/vault*; do echo $var_file; done
+
+echo Decrypt proxmox folder
 for var_file in $FOLDER/*/group_vars/*/vault*; do ansible-vault decrypt $var_file --vault-password-file $VAULT_FILE; done
